5ae8f2a5779286e1a66784cb2fe05cae67739ef1
Genvictus <johannstarkmk42@gmail.com>
2023-05-01 00:53:22 +0700

    feat: class Barang

---
+++ src/main/java/susstore/susstore/Barang.java
@@ -0,0 +1,85 @@
+package susstore.susstore;
+
+import java.awt.image.BufferedImage;
+
+public class Barang {
+    protected String namaBarang;
+    protected int stok;
+    protected String kategori;
+    protected BufferedImage gambar;
+    protected Nominal hargaBarang;
+    protected Nominal hargaBeliBarang;
+
+    public Barang(String namaBarang, String kategori, Nominal hargaBarang, Nominal hargaBeliBarang) {
+        this.namaBarang = namaBarang;
+        this.kategori = kategori;
+        this.hargaBarang = hargaBarang;
+        this.hargaBeliBarang = hargaBeliBarang;
+    }
+
+    public Barang(String namaBarang, int stok, String kategori, BufferedImage gambar, Nominal hargaBarang, Nominal hargaBeliBarang) {
+        this.namaBarang = namaBarang;
+        this.stok = stok;
+        this.kategori = kategori;
+        this.gambar = gambar;
+        this.hargaBarang = hargaBarang;
+        this.hargaBeliBarang = hargaBeliBarang;
+    }
+
+    /* Getter & Setter */
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public BufferedImage getGambar() {
+        return gambar;
+    }
+
+    public Nominal getHargaBarang() {
+        return hargaBarang;
+    }
+
+    public Nominal getHargaBeliBarang() {
+        return hargaBeliBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public void setGambar(BufferedImage gambar) {
+        this.gambar = gambar;
+    }
+
+    public void setHargaBarang(Nominal hargaBarang) {
+        this.hargaBarang = hargaBarang;
+    }
+
+    public void setHargaBeliBarang(Nominal hargaBeliBarang) {
+        this.hargaBeliBarang = hargaBeliBarang;
+    }
+
+    /* Modifier */
+    public void ambilBarang(int jumlah) throws Exception {
+        if(jumlah > this.stok) {
+            throw new Exception("Tidak cukup stok");
+        }
+        this.stok -= jumlah;
+    }
+}
