384c41b7d5fcb7c8a75d03e042655183a5466308
Genvictus <johannstarkmk42@gmail.com>
2023-05-01 20:36:14 +0700

    feat: Customer class

---
+++ src/main/java/susstore/susstore/Customer.java
@@ -0,0 +1,30 @@
+package susstore.susstore;
+public class Customer implements Storable {
+    private static int jumlahCustomer;
+    protected int id;
+    protected int jumlahTransaksi;
+
+    public Customer() {
+        this.id = jumlahCustomer;
+        Customer.jumlahCustomer++;
+        this.jumlahTransaksi = 1;
+    }
+
+    public Customer(Customer other) {
+        this.id = other.id;
+        this.jumlahTransaksi = other.jumlahTransaksi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getJumlahTransaksi() {
+        return jumlahTransaksi;
+    }
+
+    public Nominal bayar(Nominal harga) {
+        // do nothing (..yet, probably until user credit system is added)
+        return new Nominal(harga);
+    }
+}
