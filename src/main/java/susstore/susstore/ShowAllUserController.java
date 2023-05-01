53a894b59e2d60a208954d8dcc605be42b68e093
Haziq <haziqabiyyu@gmail.com>
2023-05-01 22:18:00 +0700

    Add: EditBarangController, KasirController, ShowAllUserController, UtamaController

---
+++ src/main/java/susstore/susstore/ShowAllUserController.java
@@ -0,0 +1,11 @@
+package susstore.susstore;
+
+public class ShowAllUserController implements Subscriber {
+    public ShowAllUserController() {
+
+    }
+
+    public void update() {
+        
+    }
+}
