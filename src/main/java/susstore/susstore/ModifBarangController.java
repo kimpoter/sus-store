ae63bddd6e40f9d556f8c93ed745894311254535
Haziq <haziqabiyyu@gmail.com>
2023-05-01 22:11:33 +0700

    Add: SettingsController, PluginController, ModifBarangController, GudangController

---
+++ src/main/java/susstore/susstore/ModifBarangController.java
@@ -0,0 +1,11 @@
+package susstore.susstore;
+
+public class ModifBarangController implements Subscriber {
+    public ModifBarangController() {
+        
+    }
+
+    public void update() {
+        
+    }
+}
