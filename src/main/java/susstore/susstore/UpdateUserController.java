d80fd92643632a9b68b46e765d81b900ead47d33
Haziq <haziqabiyyu@gmail.com>
2023-05-01 22:21:23 +0700

    Add: TambahUserController, UpdateUserController, TransaksiUserController, LaporanPenjualanController

---
+++ src/main/java/susstore/susstore/UpdateUserController.java
@@ -0,0 +1,11 @@
+package susstore.susstore;
+
+public class UpdateUserController implements Subscriber {
+    public UpdateUserController() {
+
+    }
+
+    public void update() {
+        
+    }
+}
