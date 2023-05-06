aff05b3e3936353c53789a4d1ed969b44056e8c9
Haziq <haziqabiyyu@gmail.com>
2023-05-06 20:49:36 +0700

    Add AllBarangController, AllCustomerController

---
+++ src/main/java/susstore/susstore/controller/pageController/AllBarangController.java
@@ -0,0 +1,14 @@
+package susstore.susstore.controller.pageController;
+
+import javafx.scene.control.Label;
+
+public class AllBarangController {
+    private Label namaBarangLabel;
+    private Label stockLabel;
+    private Label categoryLabel;
+    private Label hargaBarangLabel;
+    private Label hargaBeliBarangLabel;
+    private Label nameLabel;
+    private Label priceLabel;
+
+}
