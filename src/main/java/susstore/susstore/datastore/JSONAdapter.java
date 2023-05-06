e5e26cd369ed9d12a16f4e5d90e93a6971488576
Haziq <haziqabiyyu@gmail.com>
2023-05-06 15:08:52 +0700

    add JSONAdapter methods:

--- src/main/java/susstore/susstore/datastore/JSONAdapter.java
+++ src/main/java/susstore/susstore/datastore/JSONAdapter.java
@@ -1,5 +1,23 @@
 package susstore.susstore.datastore;
 
-public class JSONAdapter {
+public class JSONAdapter<T> extends FileAdapter<T> {
+
+    public JSONAdapter(String targetFile, Class objClass) {
+        super(targetFile, objClass);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void storeObject(T obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'storeObject'");
+    }
+
+    @Override
+    public T loadObject() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadObject'");
+    }
+
 
 }
