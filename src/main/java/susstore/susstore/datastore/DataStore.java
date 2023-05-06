32c1d9b178e9403b7cc48335a8a847e1210cb941
Genvictus <johannstarkmk42@gmail.com>
2023-05-07 00:03:25 +0700

    feat: DataStore Class for storing List of objects/datas

--- src/main/java/susstore/susstore/datastore/DataStore.java
+++ src/main/java/susstore/susstore/datastore/DataStore.java
@@ -1,22 +1,48 @@
 package susstore.susstore.datastore;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
-public class DataStore {
-    private List<Storable> data;
-    private Class type;
+public class DataStore<T> {
+    public enum TYPE {
+        JSON,
+        OBJ,
+        XML
+    }
+    private ObjectStore<T[]> store;
+    private List<T> data;
 
-    public Storable getByID(String searchId) {
-        for (Storable item: this.data
-             ) {
-            if(item.getStorableId() == searchId) {
-                return item;
-            }
+    public DataStore(String targetFile, Class<T> objClass, TYPE type) throws Exception {
+        switch (type) {
+            case JSON:
+                this.store = new JSONAdapter<T[]>(targetFile, objClass);
+                break;
+            case XML:
+                this.store = new XMLAdapter<T[]>(targetFile, objClass);
+                break;
+            case OBJ:
+                this.store = new OBJAdapter<T[]>(targetFile, objClass);
+                break;
         }
-        return null;
+        try {
+            loadData();
+        }
+        catch(Exception e) {
+            this.data = new ArrayList<T>();
+            throw e;
+        }
+    }
+    public void storeData() throws Exception {
+        this.store.storeObject((T[]) this.data.toArray());
     }
 
-    public List<Storable> getData() {
+    public void loadData() throws Exception {
+        this.data = Arrays.asList(this.store.loadObject());
+    }
+
+    public List<T> getData() {
         return data;
     }
 }
