e88ea7a366043a6742e813b073b23b05cd7a9544
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-05-07 15:59:55 +0700

    fixed: add implementation

--- src/main/java/susstore/susstore/controller/FixedBillController.java
+++ src/main/java/susstore/susstore/controller/FixedBillController.java
@@ -1,7 +1,9 @@
 package susstore.susstore.controller;
 
+import susstore.susstore.Subscriber;
 import susstore.susstore.SubscriberManager;
 import susstore.susstore.datastore.Storable;
+import susstore.susstore.models.FixedBill;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,9 +13,38 @@ public class FixedBillController {
     private SubscriberManager subscribers;
 
     // TEMPORARY LIST TO STORE
-    private List<Storable> fixedBills;
+    private List<FixedBill> fixedBills;
 
-    public FixedBillController(){
-        this.fixedBills = new ArrayList<Storable>();
+    public FixedBillController()
+    {
+        this.fixedBills = new ArrayList<FixedBill>();
+        this.subscribers = new SubscriberManager();
+    }
+
+    public FixedBillController(List<FixedBill> fixedBills)
+    {
+        this.fixedBills = fixedBills;
+        this.subscribers = new SubscriberManager();
+    }
+
+    public void setFixedBills(List<FixedBill> fixedBills)
+    {
+        this.fixedBills = fixedBills;
+    }
+
+    public void addFixedBill(FixedBill fixedBill)
+    {
+        this.fixedBills.add(fixedBill);
+        this.subscribers.notifysubs();
+    }
+
+    public List<FixedBill> getFixedBills()
+    {
+        return this.fixedBills;
+    }
+
+    public void addSubscriber(Subscriber s)
+    {
+        this.subscribers.subscribe(s);
     }
 }
