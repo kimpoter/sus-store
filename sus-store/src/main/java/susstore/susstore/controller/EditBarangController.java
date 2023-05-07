0978efc0349d67d8a1ff8ed289e67305cf430d84
farizkik <fariski.mail@gmail.com>
2023-05-08 03:24:24 +0700

    feat: edit barang

--- sus-store/src/main/java/susstore/susstore/controller/EditBarangController.java
+++ sus-store/src/main/java/susstore/susstore/controller/EditBarangController.java
@@ -1,15 +1,33 @@
 package susstore.susstore.controller;
 
 import susstore.susstore.Subscriber;
+import susstore.susstore.SubscriberManager;
+import susstore.susstore.models.Barang;
 
-import java.util.concurrent.Flow.Subscription;
-
-public class EditBarangController implements Subscriber {
+public class EditBarangController{
+    private SubscriberManager subscriberManager;
+    private Barang chosenBarang;
     public EditBarangController() {
-
+        this.subscriberManager = new SubscriberManager();
     }
 
     public void update(String s) {
 
+    }
+
+    public void addSubscriber(Subscriber s){
+        subscriberManager.subscribe(s);
+    }
+
+    public void notifysubs(String s){
+        subscriberManager.notifysubs(s);
+    }
+
+    public void setBarang(Barang b){
+        this.chosenBarang=b;
+        subscriberManager.notifysubs("choose-new-barang");
+    }
+    public Barang getBarang(){
+        return this.chosenBarang;
     }
 }
