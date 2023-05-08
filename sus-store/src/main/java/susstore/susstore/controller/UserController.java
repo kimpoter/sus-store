5c8101a22a909bf52051ab1e91e2d4a078a3c26e
Genvictus <johannstarkmk42@gmail.com>
2023-05-08 08:51:41 +0700

    feat: dataStore implementation after every CRUD user operation

--- sus-store/src/main/java/susstore/susstore/controller/UserController.java
+++ sus-store/src/main/java/susstore/susstore/controller/UserController.java
@@ -84,10 +84,18 @@ public class UserController {
 
     public void addToVIP(Member m, Integer id) {
         this.members.set(id, new MemberVIP(m));
+        try {
+            this.customerStore.storeData(new CustomerWrapper(customers, members));
+        } catch (Exception e) {
+        }
     }
 
     public void deleteFromVIP(Member m, Integer id) {
         this.members.set(id, new Member(m));
+        try {
+            this.customerStore.storeData(new CustomerWrapper(customers, members));
+        } catch (Exception e) {
+        }
     }
 
     public Integer getMemberIdxByID(UUID id) {
